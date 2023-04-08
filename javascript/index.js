a40980616cf6c7cecfcd43388b4ceb22e06c2ab3
Isabella <isabellaj.lizarde@gmail.com>
2023-04-08 01:39:54 -0700

    Updated link to homepage

--- javascript/index.js
+++ javascript/index.js
@@ -42,7 +42,7 @@ function updateCity(event) {
             "h:mm:ss"
           )}<small>${cityTime.format("A")} </small></div>
         </div>
-        <a href="/index.html">All Cities</a>
+        <a href="/">All Cities</a>
   `;
 }
 
